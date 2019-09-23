35e18ee347fcbbbf4a26397d6313c8bcf281b271
Marc Meier <Obererpel@users.noreply.github.com>
2019-09-23 18:08:32 +0200

    Maintenance Mode

--- nextcloud_backup.sh
+++ nextcloud_backup.sh
@@ -9,7 +9,6 @@ function load_config_file()
   echo "Loading config file"
   . ./$CONFIG_FILE_NAME 2> /dev/null
 
-echo "FOO"
   if [ $? -ne 0 ]; then
     echo "Could not find config file $CONFIG_FILE_NAME";
     exit ERROR_CONFIG_FILE_UNAVAILABLE;
@@ -17,4 +16,20 @@ echo "FOO"
   echo "Successfully loaded config file"
 }
 
+function set_maintenance_mode(){
+  if [ $1 -eq 1 ]; then
+    echo "Enable maintenance mode"
+    php occ maintenance:mode --on
+  else
+    echo "Disable maintenance mode"
+    php occ maintenance:mode --off
+  fi
+}
+
 load_config_file
+
+cd $nextcloud_directory
+
+set_maintenance_mode 1
+
+set_maintenance_mode 0
